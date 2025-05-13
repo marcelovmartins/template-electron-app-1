15067bc526f84a84a13ad7ad16d5cee91e683f75
Kaique Covo <kaique.kng@gmail.com>
2025-05-12 22:23:23 -0300

    feat: initialize Electron app with basic structure
    - Added package.json for project configuration and dependencies
    - Created script.js for application logic
    - Added styles.css for basic styling

---
+++ main.js
@@ -0,0 +1,23 @@
+// Importa módulos app e BrowserWindo do Electron
+const { app, BrowserWindow } = require('electron');
+
+// Função para criar a janela principal
+function createWindow() {
+
+  // Criar uma nova instância do BrowserWindow
+  const window = new BrowserWindow({
+    width: 800, // Largura da janela
+    height: 600, // Altura da janela
+  })
+
+  // Carregar o arquivo HTML na janela
+  window.loadFile('index.html');
+
+}
+
+// Evento que é acionado quando o Electron está pronto
+app.whenReady().then( () => {
+  createWindow(); // Chama função de criar a janela
+} )
+
+
